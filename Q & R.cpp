d6b776674d4c3436f5fd526c0f8315163b258bd1
ravi4246 <129968324+ravi4246@users.noreply.github.com>
2024-02-26 08:52:39 +0530

    Create Q & R.cpp

---
+++ Q & R.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int a,b,Q,R;
+
+	cout<<"Enter the Numbers 1 & 2: ";
+
+	cin>>a>>b;
+
+	Q = a / b;
+
+	R = a % b;
+
+	cout<<"Quotient: "<<Q<<endl;
+
+	cout<<"Remainder: "<<R<<endl;
+
+	return 0;
+
+}
