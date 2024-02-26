2ed33bf6480d894826d3bf9ae562cffa9c7d10ee
ravi4246 <129968324+ravi4246@users.noreply.github.com>
2024-02-26 08:57:12 +0530

    Create Sum of Numbers.cpp

---
+++ Sum of Numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a,b,sum;
+cout<<"Enter the number:";
+cin>>a;
+cout<<"Enter the Number:";
+cin>>b;
+sum=a+b;
+cout<<"The sum of two numbers is:"<<sum;
+return 0;
+}
